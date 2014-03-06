d0beaac6f154759370c9b78b0158781cd0d54802
Jonas Minnberg <sasq64@gmail.com>
2014-03-06 16:15:32 +0100

    Changed dir structure

--- module.mk
+++ module.mk
@@ -2,24 +2,24 @@ ifeq ($(GRAPPIX_INCLUDED),)
 GRAPPIX_INCLUDED = 1
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-INCLUDES += $(THIS_DIR).. $(THIS_DIR)freetype-gl
-DIRS += $(THIS_DIR) $(THIS_DIR)freetype-gl $(THIS_DIR)shaders
+INCLUDES += $(THIS_DIR) $(THIS_DIR)grappix/freetype-gl
+DIRS += $(THIS_DIR)grappix $(THIS_DIR)grappix/freetype-gl $(THIS_DIR)grappix/shaders
 CFLAGS +=  -DWITH_FREETYPE
 
 ifeq ($(HOST),android)
-  DIRS += $(THIS_DIR)android
+  DIRS += $(THIS_DIR)grappix/android
   LIBS += -lz -llog -landroid -lEGL -lGLESv2
 else ifeq ($(HOST),emscripten)
-  DIRS += $(THIS_DIR)pc
+  DIRS += $(THIS_DIR)grappix/pc
 else ifeq ($(HOST),apple)
-  DIRS += $(THIS_DIR)pc
+  DIRS += $(THIS_DIR)grappix/pc
   LIBS += -lglfw -framework OpenGL -lGLEW
 else
-  DIRS += $(THIS_DIR)pc
+  DIRS += $(THIS_DIR)grappix/pc
   LIBS += -lglfw -lGL -lGLEW
 endif
 
-include $(THIS_DIR)../freetype/module.mk
+include $(THIS_DIR)freetype/module.mk
 include $(UTILS)/coreutils/module.mk
 
 endif
