d2d238bcf2f617836d0e8f65f888c5c3acaeec2f
Jonas Minnberg <sasq64@gmail.com>
2014-04-10 12:02:58 +0200

    Rewrote tween system. Much cleaner

--- grappix/grappix/tween.cpp
+++ grappix/grappix/tween.cpp
@@ -3,65 +3,71 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-namespace grappix {
+//namespace grappix {
 
 namespace tween {
 
-double linear(double t, double s, double delta) {
-	return delta*t + s;
+double linear(double t, double delta) {
+	return delta*t;
 }
 
-double smoothStep(double t, double s, double delta) {
-	return s + delta*(t*t *(3 - 2*t));
+double smoothStep(double t, double delta) {
+	return delta*(t*t *(3 - 2*t));
 }
 
-double easeInSine (double t, double b , double c) {
-	return -c * cos(t * (M_PI/2)) + c + b;
+double easeInSine (double t, double delta) {
+	return -delta * cos(t * (M_PI/2)) + delta;
 }
 
-double easeOutSine(double t, double b ,double c) {
-	return c * sin(t * (M_PI/2)) + b;        
+double easeOutSine(double t, double delta) {
+	return delta * sin(t * (M_PI/2));        
 }
 
-double easeInOutSine(double t, double b ,double c) {
-	return -c/2 * (cos(M_PI*t) - 1) + b;
+double easeInOutSine(double t, double delta) {
+	return -delta/2 * (cos(M_PI*t) - 1);
 }
 
-double easeInBack (double t,double b , double c) {
+double easeInBack (double t, double delta) {
 	double s = 1.70158f;
-	return c*t*t*((s+1)*t - s) + b;
+	return delta*t*t*((s+1)*t - s);
 }
 
-double easeOutBack(double t,double b , double c) {        
+double easeOutBack(double t, double delta) {        
 	double s = 1.70158f;
 	t--;
-	return c*(t*t*((s+1)*t + s) + 1) + b;
+	return delta*(t*t*((s+1)*t + s) + 1);
 }
 
-double easeInOutBack(double t,double b , double c) {
+double easeInOutBack(double t, double delta) {
 	double s = 1.70158f * 1.525f;
 	//double s2 = s * 1.525f;
 	t *= 2;
-	if (t < 1) return c/2*(t*t*((s+1)*t - s)) + b;
+	if (t < 1) return delta/2*(t*t*((s+1)*t - s));
 	double postFix = t-=2;
-	return c/2*((postFix)*t*((s+1)*t + s) + 2) + b;
+	return delta/2*((postFix)*t*((s+1)*t + s) + 2);
 }
 
-std::vector<Tween> Tween::allTweens;
+std::vector<std::shared_ptr<Tween>> Tween::allTweens;
 double Tween::currentTime = 0;
 
-std::vector<NewTween> NewTween::allTweens;
-double NewTween::currentTime = 0;
-
-int to(float totalTime, const std::initializer_list<TweenAttr> &il) {
-	return Tween::to(totalTime, il);
+Tween& make_tween() {
+	Tween::allTweens.push_back(std::make_shared<Tween>());
+	return *(Tween::allTweens.back());
 }
 
-NewTween& make_tween() {
-	NewTween::allTweens.emplace_back();
-	return NewTween::allTweens.back();
+void Holder::cancel() {
+	if(!tween) return;
+	auto it = Tween::allTweens.begin();
+	//LOGD("We have %d tweens", allTweens.size());
+	while(it != Tween::allTweens.end()) {
+		if(*it == tween) {
+			LOGD("Tween canceled");
+			Tween::allTweens.erase(it);
+			break;
+		}
+		++it;
+	}
 }
 
-
-}
+//}
 }
--- grappix/grappix/tween.h
+++ grappix/grappix/tween.h
@@ -3,385 +3,146 @@
 
 #include <coreutils/log.h>
 
+#include <cmath>
 #include <functional>
 #include <vector>
+#include <limits>
+#include <memory>
 
-namespace grappix {
+//namespace grappix {
 
 namespace tween {
 
-enum TweenFuncArg {
-	ease = 0
-};
+double linear(double t, double s);
+double smoothStep(double t, double s);
+double easeInSine (double t, double b);
+double easeOutSine(double t, double b);
+double easeInOutSine(double t, double b);
+double easeInBack (double t, double b);
+double easeOutBack(double t, double b);
+double easeInOutBack(double t, double b);
 
-enum DoubleArg {
-	delay = 10 
-};
-
-enum FuncArg {
-	onComplete = 20
-};
-
-double linear(double t, double s, double delta);
-double smoothStep(double t, double s, double delta);
-double easeInSine (double t, double b , double c);
-double easeOutSine(double t, double b ,double c);
-double easeInOutSine(double t, double b ,double c);
-double easeInBack (double t,double b , double c);
-double easeOutBack(double t,double b , double c);       
-double easeInOutBack(double t,double b , double c);
-
-class TweenAttr {
+class TweenAttrBase {
 public:
-	TweenAttr(float &target, float value) : type(FLOAT), ftarget(&target), startValue(target), delta(value - target) {}
-	TweenAttr(double &target, double value) : type(DOUBLE), dtarget(&target), startValue(target), delta(value - target) {}
-	TweenAttr(int &target, int value) : type(INT), itarget(&target), startValue(target), delta(value - target) {}
-	TweenAttr(FuncArg ta, std::function<void()> f) : type(TWEENARG), arg(ta), func(f) {}
-	TweenAttr(TweenFuncArg ta, std::function<double(double, double, double)> f) : type(TWEENARG), arg(ta), tweenFunc(f) {}
-	TweenAttr(DoubleArg ta, double d) : type(TWEENARG), arg(ta), startValue(d) {}
 
-	enum Type {
-		//BYTE,
-		//SHORT,
-		INT,
-		FLOAT,
-		DOUBLE,
-		TWEENARG
-	};
+	TweenAttrBase(double target, double value) : startValue(target), delta(value - target), maxValue(std::numeric_limits<double>::max()) {}
 
-	Type type;
-	union {
-		float *ftarget;
-		double *dtarget;
-		int *itarget;
-		int arg;
-	};
+	virtual void set(double v) = 0;
 
 	double startValue;
 	double delta;
-
 	double delay;
 
-	std::function<void()> func;
-	std::function<double(double, double, double)> tweenFunc;
+	double maxValue;
 
+	std::function<void()> func;
+	std::function<double(double, double)> tweenFunc;
+
+};
+
+
+template <typename T> class TweenAttr : public TweenAttrBase {
+public:
+	TweenAttr(T &target, T value) : TweenAttrBase(target, value), target(&target) {}
+	T *target;
+
+	virtual void set(double v) override {
+		*target = static_cast<T>(v);
+	}
+};
+
+class Tween;
+
+class Holder {
+public:
+	Holder() {}
+	Holder(std::shared_ptr<Tween> t) : tween(t) {}
+	void cancel();
+private:
+	std::shared_ptr<Tween> tween;
 };
 
 class Tween {
 public:
-	Tween() : delay(0.0), tweenFunc(smoothStep) {}
+	Tween() : delay(0.0), startTime(currentTime), tweenFunc(smoothStep) {}
 
-	static int to(float totalTime, const std::initializer_list<TweenAttr> &il) {
-		Tween t;
-		t.startTime = currentTime;
-		t.totalTime = totalTime;
-		for(auto &i : il) {
-			t.add(i);
-		}
-		LOGD("Pushing tween");
-		allTweens.push_back(t);
-		return allTweens.size()-1;
+	Tween& operator=(const Tween &other) = delete;
+	Tween(const Tween& other) = delete;
+
+	operator Holder() {
+		return Holder(Tween::allTweens.back());
 	}
 
-	static int from(float totalTime, const std::initializer_list<TweenAttr> &il) {
-		Tween t;
-		t.startTime = currentTime;
-		t.totalTime = totalTime;
-		for(auto &i : il) {
-			t.add(i, true);
-		}
-		LOGD("Pushing tween");
-		allTweens.push_back(t);
-		return allTweens.size()-1;
-	}
-
-	static void update(int t, float totalTime, const std::initializer_list<TweenAttr> &il) {
-		//Tween &tween = allTweens[t];
-	}
-
-	template <class... T> static int to(float totalTime, T ... args) {
-		return toX(totalTime, { args... });
-	}
-
-	void add(const TweenAttr &ta, bool from = false) {
-
-		if(ta.type == TweenAttr::TWEENARG) {
-			switch(ta.arg) {
-			case ease:
-				tweenFunc = ta.tweenFunc;
-				break;
-			case onComplete:
-				onCompleteFunc = ta.func;
-				break;
-			case tween::delay:
-				this->delay = ta.startValue;
-				break;
-			}
-			return;
-		}
-
-		args.push_back(ta);
-		auto &a = args.back();
-		a.tweenFunc = tweenFunc;
-		a.delay = delay;
-		if(from) {
-			LOGD("Negating delta %f and startValue %f -> %f", a.delta, a.startValue, a.startValue + a.delta);
-			//startValue(target), delta(value - target)
-			a.startValue += a.delta;
-			a.delta = -a.delta;
-			switch(a.type) {
-			case TweenAttr::FLOAT:
-				*a.ftarget = a.startValue;
-				break;
-			case TweenAttr::DOUBLE:
-				*a.dtarget = a.startValue;
-				break;
-			case TweenAttr::INT:
-				*a.itarget = (int)a.startValue;
-				break;
-			case TweenAttr::TWEENARG:
-				throw std::exception();//"Incorrect arg");
-			}
-		}
-	}
-
-	bool step() { // t : 0 -> 1
-		size_t ended = 0;
-		for(auto &a : args) {
-			float t = (currentTime - startTime - a.delay) / totalTime;
-			if(t < 0.0)
-				continue;
-			if(t > 1.0) {
-				ended++;
-				switch(a.type) {
-				case TweenAttr::FLOAT:
-					*a.ftarget = a.startValue + a.delta;
-					break;
-				case TweenAttr::DOUBLE:
-					*a.dtarget = a.startValue + a.delta;
-					break;
-				case TweenAttr::INT:
-					*a.itarget = (int)(a.startValue + a.delta);
-					break;
-				case TweenAttr::TWEENARG:
-					throw std::exception();//"Incorrect arg");
-				}
-				continue;
-			}
-			switch(a.type) {
-			case TweenAttr::FLOAT:
-				*a.ftarget = a.tweenFunc(t, a.startValue, a.delta);
-				break;
-			case TweenAttr::DOUBLE:
-				*a.dtarget = a.tweenFunc(t, a.startValue, a.delta);
-				break;
-			case TweenAttr::INT:
-				*a.itarget = (int)a.tweenFunc(t, a.startValue, a.delta);
-				break;
-			case TweenAttr::TWEENARG:
-				throw std::exception();//"Incorrect arg");
-			}
-		}
-		return ended < args.size();
-	};
-
-	static void updateTweens(double t) {
-		//std::vector<std::function<void()>> callbacks;
-		currentTime = t;
-		auto it = allTweens.begin();
-		//LOGD("We have %d tweens", allTweens.size());
-		while(it != allTweens.end()) {
-			if(!it->step()) {
-				LOGD("Tween ended");
-				if(it->onCompleteFunc)
-					it->onCompleteFunc();
-				//allTweens.erase(it++);
-				it = allTweens.erase(it);
-			} else {
-				//LOGD("Tween going");
-				it++;
-			}
-		}
-	}
-
-	double startTime;
-	double totalTime;
-
-	double delay;
-
-	std::vector<TweenAttr> args;
-	std::function<double(double, double, double)> tweenFunc;
-	std::function<void()> onCompleteFunc;
-
-	static double currentTime;
-	static std::vector<Tween> allTweens;
-};
-
-
-class NewTween {
-public:
-	NewTween() : delay(0.0), startTime(currentTime), tweenFunc(smoothStep) {}
-
-	NewTween& seconds(float s) {
+	Tween& seconds(float s) {
 		totalTime = s;
 		return *this;
 	}
 
-	NewTween& speed(float s) {
+	Tween& speed(float s) {
 		dspeed = s;
 		return *this;
 	}
 
-	NewTween& linear() {
+	Tween& linear() {
 		tweenFunc = tween::linear;
 		return *this;
 	}
 
-	NewTween& to(float &target, float value) {
-		args.emplace_back(target, value);
-		auto &a = args.back();
-		a.tweenFunc = tweenFunc;
-		a.delay = delay;
-
-		//a.startValue += a.delta;
-		//a.delta = -a.delta;
-		//*a.ftarget = a.startValue;
+	template <typename T, class = typename std::enable_if<std::is_compound<T>::value>::type>
+	Tween& to(T &target, T value) {
+		for(int i=0; i<target.size(); i++) {
+			args.emplace_back(std::make_shared<TweenAttr<T>>(target[i], value[i]));
+			auto &a = args.back();
+			a->tweenFunc = tweenFunc;
+			a->delay = delay;
+		}
 		return *this;
 	}
 
-	template <typename T, class = typename std::enable_if<std::is_compound<T>::value>::type>
-	NewTween& to(T &target, T value) {
-		for(int i=0; i<2; i++) {
-			args.emplace_back(target[i], value[i]);
-			auto &a = args.back();
-			a.tweenFunc = tweenFunc;
-			a.delay = delay;
+	template <typename T, typename U> Tween& to(T &target, U value, int cycles = 1) {
+		args.emplace_back(std::make_shared<TweenAttr<T>>(target, value));
+		auto &a = args.back();
+
+		if(cycles != 1) {
+			a->maxValue = a->delta+1;
+			a->delta = (a->delta+1)*cycles-1;
 		}
 
-		//a.startValue += a.delta;
-		//a.delta = -a.delta;
-		//*a.ftarget = a.startValue;
+		a->tweenFunc = tweenFunc;
+		a->delay = delay;
 		return *this;
 	}
 
-	NewTween& on_complete(std::function<void()> f) {
+
+	template <typename T, typename U> Tween& from(T &target, U value) {
+
+		to(target, value);
+		auto &a = args.back();
+
+		LOGD("Negating delta %f and startValue %f -> %f", a->delta, a->startValue, a->startValue + a->delta);
+		a->startValue += a->delta;
+		a->delta = -a->delta;
+		a->set(a->startValue);
+		return *this;
+	}
+
+	Tween& on_complete(std::function<void()> f) {
 		onCompleteFunc = f;
 		return *this;
-	}
-
-/*
-	static int to(float totalTime, const std::initializer_list<TweenAttr> &il) {
-		NewTween t;
-		t.startTime = currentTime;
-		t.totalTime = totalTime;
-		for(auto &i : il) {
-			t.add(i);
-		}
-		LOGD("Pushing tween");
-		allTweens.push_back(t);
-		return allTweens.size()-1;
-	}
-
-	static int from(float totalTime, const std::initializer_list<TweenAttr> &il) {
-		NewTween t;
-		t.startTime = currentTime;
-		t.totalTime = totalTime;
-		for(auto &i : il) {
-			t.add(i, true);
-		}
-		LOGD("Pushing tween");
-		allTweens.push_back(t);
-		return allTweens.size()-1;
-	}
-
-	static void update(int t, float totalTime, const std::initializer_list<TweenAttr> &il) {
-		//Tween &tween = allTweens[t];
-	}
-
-	template <class... T> static int to(float totalTime, T ... args) {
-		return toX(totalTime, { args... });
-	}
-*/
-
-
-	void add(const TweenAttr &ta, bool from = false) {
-
-		if(ta.type == TweenAttr::TWEENARG) {
-			switch(ta.arg) {
-			case ease:
-				tweenFunc = ta.tweenFunc;
-				break;
-			case onComplete:
-				onCompleteFunc = ta.func;
-				break;
-			case tween::delay:
-				this->delay = ta.startValue;
-				break;
-			}
-			return;
-		}
-
-		args.push_back(ta);
-		auto &a = args.back();
-		a.tweenFunc = tweenFunc;
-		a.delay = delay;
-		if(from) {
-			LOGD("Negating delta %f and startValue %f -> %f", a.delta, a.startValue, a.startValue + a.delta);
-			//startValue(target), delta(value - target)
-			a.startValue += a.delta;
-			a.delta = -a.delta;
-			switch(a.type) {
-			case TweenAttr::FLOAT:
-				*a.ftarget = a.startValue;
-				break;
-			case TweenAttr::DOUBLE:
-				*a.dtarget = a.startValue;
-				break;
-			case TweenAttr::INT:
-				*a.itarget = (int)a.startValue;
-				break;
-			case TweenAttr::TWEENARG:
-				throw std::exception();//"Incorrect arg");
-			}
-		}
 	}
 
 	bool step() { // t : 0 -> 1
 		size_t ended = 0;
 		for(auto &a : args) {
-			float t = (currentTime - startTime - a.delay) / totalTime;
+			float t = (currentTime - startTime - a->delay) / totalTime;
 			if(t < 0.0)
 				continue;
 			if(t > 1.0) {
 				ended++;
-				switch(a.type) {
-				case TweenAttr::FLOAT:
-					*a.ftarget = a.startValue + a.delta;
-					break;
-				case TweenAttr::DOUBLE:
-					*a.dtarget = a.startValue + a.delta;
-					break;
-				case TweenAttr::INT:
-					*a.itarget = (int)(a.startValue + a.delta);
-					break;
-				case TweenAttr::TWEENARG:
-					throw std::exception();//"Incorrect arg");
-				}
+				a->set(a->startValue + fmod(a->delta, a->maxValue));
 				continue;
 			}
-			switch(a.type) {
-			case TweenAttr::FLOAT:
-				*a.ftarget = a.tweenFunc(t, a.startValue, a.delta);
-				break;
-			case TweenAttr::DOUBLE:
-				*a.dtarget = a.tweenFunc(t, a.startValue, a.delta);
-				break;
-			case TweenAttr::INT:
-				*a.itarget = (int)a.tweenFunc(t, a.startValue, a.delta);
-				break;
-			case TweenAttr::TWEENARG:
-				throw std::exception();//"Incorrect arg");
-			}
+			a->set(a->startValue + fmod(a->tweenFunc(t, a->delta), a->maxValue));
 		}
 		return ended < args.size();
 	};
@@ -392,10 +153,9 @@ public:
 		auto it = allTweens.begin();
 		//LOGD("We have %d tweens", allTweens.size());
 		while(it != allTweens.end()) {
-			if(!it->step()) {
-				LOGD("Tween ended");
-				if(it->onCompleteFunc)
-					callbacks.push_back(it->onCompleteFunc);
+			if(!(*it)->step()) {
+				if((*it)->onCompleteFunc)
+					callbacks.push_back((*it)->onCompleteFunc);
 				//allTweens.erase(it++);
 				it = allTweens.erase(it);
 			} else {
@@ -413,21 +173,21 @@ public:
 	double totalTime;
 	double dspeed;
 
-	std::vector<TweenAttr> args;
-	std::function<double(double, double, double)> tweenFunc;
+	std::vector<std::shared_ptr<TweenAttrBase>> args;
+	std::function<double(double, double)> tweenFunc;
 	std::function<void()> onCompleteFunc;
 
 	static double currentTime;
-	static std::vector<NewTween> allTweens;
+	static std::vector<std::shared_ptr<Tween>> allTweens;
 };
 
-int to(float totalTime, const std::initializer_list<TweenAttr> &il);
+//int to(float totalTime, const std::initializer_list<TweenAttr> &il);
 
 
-NewTween& make_tween();
+Tween& make_tween();
 
 
 }
-}
+//}
 
 #endif // TWEEN_H
