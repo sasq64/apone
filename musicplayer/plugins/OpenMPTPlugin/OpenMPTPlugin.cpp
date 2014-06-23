17242616770032e9622d5c51ddaaa6eaeb8d8609
Jonas Minnberg <sasq64@gmail.com>
2014-06-23 08:42:17 +0200

    Added ft extension to OpenMPT

--- musicplayer/plugins/OpenMPTPlugin/OpenMPTPlugin.cpp
+++ musicplayer/plugins/OpenMPTPlugin/OpenMPTPlugin.cpp
@@ -89,10 +89,11 @@ private:
 	openmpt_module *mod;
 };
 
-static const set<string> supported_ext { "mod", "xm", "s3m" , "oct", /*"okt", "okta", sucks here, use UADE */ "it", "ft", "far", "ult", "669", "dmf", "mdl", "stm", "okt", "gdm", "mt2", "mtm", "j2b", "imf", "ptm", "ams" };
+//static const set<string> supported_ext { "mod", "xm", "s3m" , "oct", /*"okt", "okta", sucks here, use UADE */ "it", "ft", "far", "ult", "669", "dmf", "mdl", "stm", "okt", "gdm", "mt2", "mtm", "j2b", "imf", "ptm", "ams" };
 
 bool OpenMPTPlugin::canHandle(const std::string &name) {
 	auto ext = utils::path_extension(name);
+	if(ext == "ft") return true;
 	LOGD("EXT %s", openmpt_get_supported_extensions());
 	return openmpt_is_extension_supported(ext.c_str());
 	//return supported_ext.count(utils::path_extension(name)) > 0;
@@ -108,64 +109,3 @@ ChipPlayer *OpenMPTPlugin::fromFile(const std::string &fileName) {
 };
 
 }
-
-/*
-JNIEXPORT jint JNICALL Java_com_ssb_droidsound_plugins_OpenMPTPlugin_N_1getIntInfo(JNIEnv *env, jobject obj, jlong song, jint what)
-{
-	openmpt_module* mod = (openmpt_module*)song;
-	switch(what)
-	{
-		case INFO_LENGTH:
-			return openmpt_module_get_duration_seconds(mod) * 1000;
-		case INFO_SUBTUNES:
-			return openmpt_module_get_num_subsongs(mod);
-		case INFO_STARTTUNE:
-			return 0;
-		case INFO_CHANNELS:
-			return openmpt_module_get_num_channels(mod);
-		case INFO_PATTERNS:
-			return openmpt_module_get_num_patterns(mod);
-		
-	}
-	return -1; 
-}
-JNIEXPORT jstring JNICALL Java_com_ssb_droidsound_plugins_OpenMPTPlugin_N_1getStringInfo(JNIEnv *env, jobject obj, jlong song, jint what)
-{
-	openmpt_module* mod = (openmpt_module*)song;
-	switch(what)
-	{
-		case INFO_INSTRUMENTS:
-			{
-						
-			char instruments[2048];
-			char *ptr = instruments;
-			char *instEnd = instruments + sizeof(instruments) - 48;
-			int pat = openmpt_module_get_num_patterns(mod);
-			int ns = openmpt_module_get_num_samples(mod);
-			memset(ptr, 0, 2048);
-			if ((ns * 48) > 2048)
-				return  NewString(env, instruments); 
-			
-			if(ns > 0)
-			{
-				for(int i=1; i<ns; i++)
-				{
-					const char* instr_name = openmpt_module_get_sample_name(mod, i);
-					memcpy((void*)ptr, instr_name, strlen(instr_name));
-					ptr += strlen(instr_name);
-					if(ptr >= instEnd)
-						break;
-					*ptr++ = 0x0a;
-					*ptr = 0;
-				}
-			}
-			return  NewString(env, instruments); 
-			}
-		
-		return NewString(env, ""); 
-	
-	}
-
-}
-
-*/
