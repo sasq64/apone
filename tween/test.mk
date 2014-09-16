79b0e9a22ebb9c22e6ee660c19f95e5934bb3fc3
Jonas Minnberg <sasq64@gmail.com>
2014-09-16 14:10:02 +0200

    Tween test makefile

---
+++ tween/test.mk
@@ -0,0 +1,29 @@
+TWEEN_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+$(warning $(TWEEN_DIR))
+
+include $(TWEEN_DIR)../config.mk
+
+include $(TWEEN_DIR)module.mk
+
+CFLAGS += -DTWEEN_UNIT_TEST
+
+
+ifeq ($(TARGET),)
+TARGET := tweentest
+endif
+
+LOCAL_FILES += 
+CFLAGS += -I$(realpath $(TWEEN_DIR)../test)
+FILES += $(realpath $(TWEEN_DIR)../test/catch.cpp)
+
+define TESTMOD_template
+FILES += $$(realpath $($(1)_FILES))
+DIRS += $$(realpath $($(1)_DIRS))
+endef
+## Generate the rules
+$(foreach mod,$(MODULES),$(eval $(call TESTMOD_template,$(mod))))
+
+MODULES :=
+
+include $(TWEEN_DIR)/../build.mk
