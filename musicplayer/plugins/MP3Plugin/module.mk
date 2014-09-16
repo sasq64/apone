30fc4a38ce63c6e3c13ca09988473fe3907794a3
Jonas Minnberg <sasq64@gmail.com>
2014-09-16 14:14:01 +0200

    New MP3 plugin

---
+++ musicplayer/plugins/MP3Plugin/module.mk
@@ -0,0 +1,15 @@
+ifeq ($(MP3PLUGIN_INCLUDED),)
+MP3PLUGIN_INCLUDED = 1
+THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+#modplug_DIRS := $(THIS_DIR)modplug
+mp3plugin_FILES := $(THIS_DIR)MP3Plugin.cpp
+mp3plugin_INCLUDES := $(THIS_DIR)../.. $(MODULE_DIR)
+
+LIBS += -lmpg123
+
+INCLUDES += $(THIS_DIR)/..
+
+MODULES += mp3plugin
+
+endif
