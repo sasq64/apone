d4820a6b05884c2b70f466e993717d5ffedd6739
Jonas Minnberg <sasq64@gmail.com>
2015-01-23 16:32:33 +0100

    More compiler fixes

--- cmake/RaspberryPi.cmake
+++ cmake/RaspberryPi.cmake
@@ -3,11 +3,24 @@ set(CMAKE_SYSTEM_NAME Linux)
 #this one not so much
 set(CMAKE_SYSTEM_VERSION 1)
 
-set(PLATFORMS arm-linux-gnueabihf 
+set(PLATFORMS arm-linux-gnueabihf arm-none-gnueabi)
+
+foreach(PLATFORM ${PLATFORMS})
+  find_path(RPI_COMPILER ${PLATFORM}-gcc)
+  message(${RPI_COMPILER})
+  if(RPI_COMPILER)
+  	get_filename_component(PI_SDK_ROOT ${RPI_COMPILER} DIRECTORY)
+  	set(PREF "${PLATFORM}-")
+  	break()
+  endif()
+ endforeach()
+
+message("PI AT " ${PI_SDK_ROOT})
+
 #set(PREF arm-linux-gnueabihf-)
 #set(PI_SDK_ROOT /opt/raspberry/arm-bcm2708/gcc-linaro-${PREF}raspbian)
-set(PREF arm-none-linux-gnueabi-)
-set(PI_SDK_ROOT /opt/raspberry/arm-none-linux-gnueabi)
+#set(PREF arm-none-linux-gnueabi-)
+#set(PI_SDK_ROOT /opt/raspberry/arm-none-linux-gnueabi)
 
 get_filename_component(PI_SYS_ROOT ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 get_filename_component(PI_SYS_ROOT "${PI_SYS_ROOT}/sysroots/piroot" ABSOLUTE)
@@ -21,14 +34,14 @@ set(CMAKE_CXX_COMPILER ${PI_SDK_ROOT}/bin/${PREF}g++)
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH ${PI_SYS_ROOT})
 
-set(CMAKE_C_FLAGS "-DGL_ES -DRASPBERRYPI -DLINUX -march=armv6 -mfpu=vfp -mfloat-abi=hard -I${PI_SYS_ROOT}/include -I${PI_SYS_ROOT}/vc/include")
-set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
+set(CMAKE_C_FLAGS "-DGL_ES -DRASPBERRYPI -DLINUX -march=armv6 -mfpu=vfp -mfloat-abi=hard -I${PI_SYS_ROOT}/include -I${PI_SYS_ROOT}/vc/include" CACHE STRING "")
+set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS} CACHE STRING "")
 set(CMAKE_C_FLAGS_RELEASE ${CMAKE_C_FLAGS})
 set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_C_FLAGS})
 set(CMAKE_C_FLAGS_DEBUG ${CMAKE_C_FLAGS})
 set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_C_FLAGS})
 
-set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++ -static-libgcc -L${PI_SYS_ROOT}/lib -L${PI_SYS_ROOT}/vc/lib")
+set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++ -static-libgcc -L${PI_SYS_ROOT}/lib -L${PI_SYS_ROOT}/vc/lib" CACHE STRING "")
 
 set(RASPBERRY 1)
 
