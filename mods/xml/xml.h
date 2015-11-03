041a4fb410563c75e0fa5270820d88975dd11daf
Jonas Minnberg <sasq64@gmail.com>
2015-11-03 21:24:37 +0100

    Improved xml parser

--- mods/xml/xml.h
+++ mods/xml/xml.h
@@ -2,7 +2,7 @@
 #define APONE_XML_H
 
 #include "tinyxml2.h"
-
+#include <coreutils/file.h>
 #include <memory>
 #include <string>
 
@@ -63,6 +63,14 @@ private:
 };
 
 
+class xml_exception : public std::exception {
+public:
+	xml_exception(const char *ptr = "XML Exception") : msg(ptr) {}
+	virtual const char *what() const throw() { return msg; }
+private:
+	const char *msg;
+};
+
 
 class xmlnode {
 public:
@@ -70,52 +78,62 @@ public:
 	xmlnode(tinyxml2::XMLNode *node) : node(node) {}
 
 	xmlnode operator[](const std::string &tag) const {
+		if(!node)
+			throw xml_exception("Invalid node");
 		return xmlnode(node->FirstChildElement(tag.c_str()));
 	}
 
+	bool valid() { return node != nullptr; }
+
 	class const_iterator  {
-		public:
-			const_iterator(tinyxml2::XMLNode *node, const char *name = nullptr) : node(node), name(name) {}
-			const_iterator(const const_iterator& rhs) : node(rhs.node), name(rhs.name) {}
+	public:
+		const_iterator(tinyxml2::XMLNode *node, const char *name = nullptr) : node(node), name(name) {}
+		const_iterator(const const_iterator& rhs) : node(rhs.node), name(rhs.name) {}
 
-			bool operator!= (const const_iterator& other) const {
-		    	return node != other.node;
-			}
-
-		    xmlnode operator* () const {
-		    	return xmlnode(node);
-		    }
-
-			const const_iterator& operator++ () {
-				node = node->NextSiblingElement(name);
-				return *this;
-			}
-		private:
-			tinyxml2::XMLNode *node;
-			const char *name;
-		};
-
-		class xmlnodelist {
-		public:
-			xmlnodelist(tinyxml2::XMLNode *node, const std::string &name = "") : node(node), name(name) {}
-			const_iterator begin() const { return const_iterator(node, name == "" ? nullptr : name.c_str()); }
-			const_iterator end() const { return const_iterator(nullptr); } 
-		private:
-			tinyxml2::XMLNode *node;
-			std::string name;
-		};
-
-		xmlnodelist all(const std::string &name = "") const {
-			return xmlnodelist(node->FirstChildElement(name.c_str()), name);
+		bool operator!= (const const_iterator& other) const {
+			return node != other.node;
 		}
 
-		std::string attr(const std::string &name) const {
-			return ((tinyxml2::XMLElement*)node)->Attribute(name.c_str());
+		xmlnode operator* () const {
+			return xmlnode(node);
 		}
 
-		std::string text() const {
-			return ((tinyxml2::XMLElement*)node)->GetText();
+		const const_iterator& operator++ () {
+			node = node->NextSiblingElement(name);
+			return *this;
 		}
+	private:
+		tinyxml2::XMLNode *node;
+		const char *name;
+	};
+
+	class xmlnodelist {
+	public:
+		xmlnodelist(tinyxml2::XMLNode *node, const std::string &name = "") : node(node), name(name) {}
+		const_iterator begin() const { return const_iterator(node, name == "" ? nullptr : name.c_str()); }
+		const_iterator end() const { return const_iterator(nullptr); } 
+	private:
+		tinyxml2::XMLNode *node;
+		std::string name;
+	};
+
+	xmlnodelist all(const std::string &name = "") const {
+		if(!node)
+			throw xml_exception("Invalid node");
+		return xmlnodelist(node->FirstChildElement(name.c_str()), name);
+	}
+
+	std::string attr(const std::string &name) const {
+		if(!node)
+			throw xml_exception("Invalid node");
+		return ((tinyxml2::XMLElement*)node)->Attribute(name.c_str());
+	}
+
+	std::string text() const {
+		if(!node)
+			throw xml_exception("Invalid node");
+		return ((tinyxml2::XMLElement*)node)->GetText();
+	}
 
 
 protected:
@@ -126,6 +144,11 @@ protected:
 class xmldoc : public xmlnode {
 public:
 	static xmldoc fromText(const std::string &text) {
+		return xmldoc(text);
+	}
+
+	static xmldoc fromFile(utils::File file) {
+		auto text = file.read();
 		return xmldoc(text);
 	}
 
